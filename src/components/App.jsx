513dbe50414dcec88d043bdc79353086a932eb96
iamSK77 <sk77.mobilefix@gmail.com>
2020-07-29 13:37:40 +0530

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import EmojiCard from "./EmojiCard";
+import dictionary from "../dictionary";
+
+import "./index.css";
+
+function createCard(card) {
+    return (
+        <EmojiCard 
+            emoji={card.emoji}
+            name={card.name}
+            description={card.meaning}
+        />
+    );
+}
+
+function App() {
+    return (
+        <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {dictionary.map(createCard)}
+      </dl>
+
+
+      </div>
+    );
+}
+
+export default App;
