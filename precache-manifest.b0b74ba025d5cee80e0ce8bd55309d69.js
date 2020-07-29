4eb342a5d766821a20bebed2e45ddae33138c84c
iamSK77 <sk77.mobilefix@gmail.com>
2020-07-29 13:51:57 +0530

    Updates

---
+++ precache-manifest.b0b74ba025d5cee80e0ce8bd55309d69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617bbc185f02793c4578d51d2479c64d",
+    "url": "/emojipedia-react/index.html"
+  },
+  {
+    "revision": "60524a895d6d969ca590",
+    "url": "/emojipedia-react/static/css/main.6bb1daae.chunk.css"
+  },
+  {
+    "revision": "78148f6365a3ab6a92d4",
+    "url": "/emojipedia-react/static/js/2.c250aed4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emojipedia-react/static/js/2.c250aed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60524a895d6d969ca590",
+    "url": "/emojipedia-react/static/js/main.394e7118.chunk.js"
+  },
+  {
+    "revision": "384f679bb65476613ca8",
+    "url": "/emojipedia-react/static/js/runtime-main.c946df2c.js"
+  }
+]);
